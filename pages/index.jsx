bdf9b4242c6105956e114f7d7db43b9d160d38c6
Tosin Owoseni <tosinowoseni1@icloud.com>
2023-01-04 21:14:37 -0500

    Made Changes to Project

---
+++ pages/index.jsx
@@ -0,0 +1,60 @@
+import "../styles/main.module.css"
+
+
+
+export default function HomePage (){
+    return (
+        <>
+        <header>
+        <nav>
+            <ul class="nav-list">
+                <li><a href="#">Home</a></li>
+                <li><a href="#">About</a></li>
+                <li><a href="#">Service information</a></li>
+                <li><a href="#">Blog</a></li>
+                <li><a href="#">Contact</a></li>
+
+            </ul>
+            
+        </nav>
+        <div>Men's | Women's | Children </div>
+        <div class="container">
+        </div>
+    </header>
+    <main>
+        <p>
+            Free Shipping for Platinum & Gold members. Free Shipping at $25 for all other Shoppers. Details
+            <img src="./images/4465172829695578598.jpeg" />
+            <img src="./images/C2202563_102_1487723.webp" />
+            <img src="./images/C2202563_101_1487716 (1).webp" />
+        </p>
+        <section class="home">
+            <img src="./images/hmgoepprod.jpeg" />
+            <img src="./images/3212_03.webp" />
+            <img src="./images/HM-Jeans-Redesign-2020-Mens-Collection-005.jpeg" />
+            <img src="./images/HM-Jeans-Redesign-2020-Mens-Collection-004.jpeg" />
+            <img src="./images/holiday_promo.webp" />
+            <h1>Welcome to this Fashion site!!</h1>
+            <p>
+                You'll be blown away by everything you buy from us. They are the best.
+                This Paragraph is some text that tells you why. Here at H&M we value
+                membership and trend culture. Become a member today to enter a world of
+                fashion with benefits. Earn points, get exclusive offers, special shopping
+                event invites and more!
+            </p>
+            <button>Sign Up</button>
+
+        </section>
+        <section class="about">
+        <h2>Who are we?</h2>
+        <p>
+            This is some text about the company. It'll give you all the
+            information you need to know about who we are so you trust us
+            when you buy from us.
+        </p>
+        </section>
+    </main>
+    <footer>H&m Fashion Website 2022</footer>
+    </>
+    )
+}
